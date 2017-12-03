beed72d618e57e72f1326a0ba2b2df427c73e1a7
gomi_ningen <gomi2ngen@gmail.com>
2017-12-03 21:21:02 +0900

    Config test for golang and rust

--- rust/src/echo.rs
+++ rust/src/echo.rs
@@ -2,15 +2,12 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let mut i: i32 = 0;
-    for v in args.iter() {
+    for (i, v) in args.iter().enumerate() {
         if i == 0 {
-            i += 1;
             continue;
         }
         let sp = if i == 1 { "" } else { " " };
         print!("{}{}", sp, v);
-        i += 1;
     }
     print!("\n");
 }
