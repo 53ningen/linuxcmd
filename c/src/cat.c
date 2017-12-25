bea0f6f093c2dcbd02fe66d5ee72fc37fc8a9f97
gomi_ningen <gomi2ngen@gmail.com>
2017-12-25 22:14:18 +0900

    Update c/src/cat.c

--- c/src/cat.c
+++ c/src/cat.c
@@ -5,14 +5,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#define BUFFER_SIZE 2048
+
+static void do_stdin_cat();
 static void do_cat(const char *path);
 static void die(const char *s);
 
 int main(int argc, char *argv[]) {
     int i;
     if (argc < 2) {
-        fprintf(stderr, "%s: file name not given\n", argv[0]);
-        exit(1);
+        do_stdin_cat();
     }
     for (i = 1; i < argc; i++) {
         do_cat(argv[i]);
@@ -20,7 +22,16 @@ int main(int argc, char *argv[]) {
     exit(0);
 }
 
-#define BUFFER_SIZE 2048
+static void do_stdin_cat() {
+    int fd;
+    unsigned char buf[BUFFER_SIZE];
+    int n;
+    for(;;) {
+        n = read(STDIN_FILENO, buf, sizeof buf);
+        if (n < 0) die("");
+        write(STDOUT_FILENO, buf, n);
+    }
+}
 
 static void do_cat(const char *path) {
     int fd;
