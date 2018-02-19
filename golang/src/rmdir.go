431e131764735f0693ad35815ab9d7bbbcac67ee
gomi_ningen <gomi2ngen@gmail.com>
2018-02-19 16:54:44 +0900

    Add rmdir command in golang

---
+++ golang/src/rmdir.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("%s: file name not given\n", os.Args[0])
+		return
+	}
+	for i, v := range os.Args {
+		if i == 0 {
+			continue
+		}
+		doRmDir(v)
+	}
+}
+
+func doRmDir(path string) error {
+	if err := os.Remove(path); err != nil {
+		die(err)
+	}
+	return nil
+}
+
+func die(e error) {
+	fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+	os.Exit(1)
+}
