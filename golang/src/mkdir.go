e8a99d9b42d07694395a7e4a5e31e4454035073e
gomi_ningen <gomi2ngen@gmail.com>
2018-02-19 16:42:51 +0900

    Add mkdir command in golang

---
+++ golang/src/mkdir.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("%s: file name not given\n", os.Args[0])
+		return
+	}
+	for i, v := range os.Args {
+		if i == 0 {
+			continue
+		}
+		doMkDir(v)
+	}
+}
+
+func doMkDir(path string) error {
+	if err := os.Mkdir(path, 0777); err != nil {
+		die(err)
+	}
+	return nil
+}
+
+func die(e error) {
+	fmt.Fprintf(os.Stderr, "%s\n", e.Error())
+	os.Exit(1)
+}
